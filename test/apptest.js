fc4a0403b9ec61ff0bc690dde9e64775c6442484
Yasir Bilal <yasirbilal22@gmail.com>
2021-10-18 17:06:28 +0500

    Initial commit

---
+++ test/apptest.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const app = require('../app')
+
+describe ('App', function(){
+    it('app should return hello', function(){
+        assert.equal(app(),'hello')
+    })
+})
